cb30d3d829f1a48ad55fdc3e69ce8e7f6db0cf6f
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-02-24 14:24:49 +0100

    changing directories

---
+++ ironhack /PreWork/ JavaScript  Functions, Data Types, Loops & Conditionals.js
@@ -0,0 +1,20 @@
+function sortPdoducts (matrix) {
+  if (matrix === "product: MacBook, price: 1019, category: tech,",
+       "product: Cheerios, price: 5, category: food,",
+   "product: Snickers, price: 1.5 , category: food,",
+       "product: Air Pods, price: 129, category: tech,")
+       
+       {
+         
+    console.log ("tech: [ { product: t1, price: 123, category: 'tech'}, { product: t2, price: 910, category: tech}],
+        "food: [ { product: f1, price: 45, category: 'food'}, { product: f2, price: 6.78 , category: food]");
+  
+ 
+  }
+}
+
+sortPdoducts (matrix)
+
+
+
+
---
+++ ironhack /PreWork/Arrays, Objects & Conditionals.js
@@ -0,0 +1,20 @@
+function filterPizzas (pizzas) {
+    if (pineaple) { 
+    
+    }
+    
+    let saboresDePizzas =[
+        { type: 'pineapple', size: 'M'},
+        { type: 'pineapple', size: 'S'},
+        { type: 'pepperoni', size: 'S'},
+        { type: 'mushrooms', size: 'L'}
+      ];
+      // criar um if case, se for pizza de pineaple volta falso. 
+      if (saboresDePizzas) {
+            return ([   { type: 'pepperoni', size: 'S'}, { type: 'mushrooms', size: 'L'} ]);
+      }else{
+            return( "pizzas erradas");
+    
+      }
+    };
+    filterPizzas();
